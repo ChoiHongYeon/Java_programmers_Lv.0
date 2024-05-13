51a5c4730dc96b02d96d4c6d9e7dd1aa4de5c9dc
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-05-13 21:58:24 +0900

    second commit

--- _20240513_1.java
+++ _20240513_1.java
@@ -5,20 +5,20 @@ class Solution {
     public int[] solution(int[] arr, int[] query) {
 
         int answer_len = arr.length;
-        int odd_sum = 0;
+        int start_num = 0;
         int i = 0;
 
-        for (int q : query) {
-            if (q % 2 == 0)
-                answer_len = q + 1;
+        for (int j = 0; j < query.length; j++) {
+            if (j % 2 == 0)
+                answer_len = query[j] + 1;
             else {
-                answer_len -= q;
-                odd_sum += q;
+                answer_len -= query[j];
+                start_num += query[j];
             }
         }
 
         int[] answer = new int[answer_len];
-        for (int j = odd_sum; j < odd_sum + answer_len; j++) {
+        for (int j = start_num; j < start_num + answer_len; j++) {
             answer[i++] = arr[j];
         }
 
@@ -28,8 +28,10 @@ class Solution {
 
 public class _20240513_1 {
     public static void main(String[] args) {
+
         Solution s = new Solution();
         int[] result = s.solution(new int[] {0,1,2,3,4,5}, new int[] {4,1,2});
+
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
